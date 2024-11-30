4ffe7457f204d04f907d3c8124b68f2843f4b1b5
liatco414 <liat667788@gmail.com>
2024-11-30 21:11:33 +0200

    index
    fixing the hamburger

--- portfolioProject/index_html/index.js
+++ portfolioProject/index_html/index.js
@@ -1,5 +1,11 @@
 const hamburger = document.querySelector('.hamburger');
+const menu = document.getElementById('menu');
+const links = document.querySelectorAll('#menu a');
 
 hamburger.addEventListener('click', () => {
     hamburger.style.display = 'block';
 })
+links.forEach(link => {
+    link.addEventListener('click', () => {
+    });
+});
